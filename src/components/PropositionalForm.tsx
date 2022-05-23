a256b4a1d5d55c38f08747c903d1a322fb3c0498
Victor Kauan <victorkauan.alu.lmb@gmail.com>
2022-05-23 13:29:47 -0300

    feat: create not operator validation

--- src/components/PropositionalForm.tsx
+++ src/components/PropositionalForm.tsx
@@ -8,14 +8,40 @@ export function PropositionalForm() {
     event.preventDefault();
   }
 
-  function onlyValidCharacters(proposition: string) {
-    const validCharacters = /^[A-Z v\~\^\-\<\>\(\)]+$/;
-    return validCharacters.test(proposition);
-  }
+  const validations = {
+    validCharacters: (formula: string) => {
+      const validCharacters = /^[A-Z v\~\^\-\<\>\(\)]+$/;
+      return validCharacters.test(formula);
+    },
+    not: (formula: string) => {
+      let formulaArray = formula.split('');
+
+      let notIndex = formulaArray.indexOf('~');
+      if (notIndex === -1) return true;
+
+      const uppercaseLetters = /[A-Z]/;
+      while (notIndex !== -1) {
+        const nextIsALetter = uppercaseLetters.test(formulaArray[notIndex + 1]);
+        if (!nextIsALetter) return false;
+
+        formulaArray.splice(notIndex, 1);
+        notIndex = formulaArray.indexOf('~');
+      }
+
+      return true;
+    },
+  };
 
   function testProposition() {
-    const propositionString = userProposition.replace(/ /g, '');
-    if (!onlyValidCharacters(propositionString)) return false;
+    const formulaString = userProposition.replace(/ /g, '');
+
+    if (
+      validations.validCharacters(formulaString) &&
+      validations.not(formulaString)
+    )
+      return true;
+
+    return false;
   }
 
   return (
